8677eca66237ff4fd265d75d59134d26f0da93d3
tilz0R <tilen.majerle@gmail.com>
2018-09-01 12:38:59 +0200

    Update examples

--- docs/examples/_example_config_template.h
+++ docs/examples/_example_config_template.h
@@ -5,9 +5,6 @@
  * Rename this file to "esp_config.h" for your application
  */
 
-/* First include debug */
-#include "esp/esp_debug.h"
-
 /*
  * Open "include/esp/esp_config_default.h" and 
  * copy & replace settings you want to change here
