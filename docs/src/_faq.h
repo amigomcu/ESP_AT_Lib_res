3c57bdb0b1e268ca3d463bd5dd582b086ddd925b
tilz0R <tilen.majerle@gmail.com>
2018-11-24 19:30:51 +0100

    Minimum version is 1.7.0

--- docs/src/_faq.h
+++ docs/src/_faq.h
@@ -6,7 +6,7 @@
  *
  * This library follows latest AT official releases available on official Espressif website.
  *
- *  - `1.6.0` AT command version is mandatory: https://www.espressif.com/en/products/software/esp-at/resource
+ *  - `1.7.0` AT command version is mandatory: https://www.espressif.com/en/products/software/esp-at/resource
  * 	- If you are not sure about your version, use `AT+GMR\r\n` command to test for current AT version
  *
  * \note            In case you do not fulfill these part, please update software on ESP to latest. Check \ref page_update_at_software section.
