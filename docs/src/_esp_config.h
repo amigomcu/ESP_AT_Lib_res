4e624e558105bcf18ba8e6763f412c3313bbdc52
tilz0R <tilen.majerle@gmail.com>
2018-12-24 18:12:14 +0100

    Add latest lib + config docs

--- docs/src/_esp_config.h
+++ docs/src/_esp_config.h
@@ -2,7 +2,9 @@
  * \addtogroup      ESP_CONF
  * \{
  *
+ * List of all possible library configuration settings.
  *
+ * Please refer to \ref sect_config for more information how to setup custom config.
  *
  * \}
  */
