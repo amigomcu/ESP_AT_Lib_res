c48240582d1e89f807919e95410bc002e1059a6a
tilz0R <tilen.majerle@st.com>
2019-02-05 11:47:58 +0100

    Fix system clock config function

--- examples/stm32f769i-discovery/netconn_server_rtos/src/main.c
+++ examples/stm32f769i-discovery/netconn_server_rtos/src/main.c
@@ -147,37 +147,43 @@ LL_Init(void) {
  */
 void
 SystemClock_Config(void) {
+	/* Configure flash latency */
     LL_FLASH_SetLatency(LL_FLASH_LATENCY_7);
     if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_7) {
-        while (1) { }  
+        while (1) {}
     }
+	
+	/* Set voltage scaling */
     LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
     LL_PWR_EnableOverDriveMode();
+	
+	/* Enable HSI */
     LL_RCC_HSI_SetCalibTrimming(16);
     LL_RCC_HSI_Enable();
-
-    /* Wait till HSI is ready */
-    while (LL_RCC_HSI_IsReady() != 1) { }
+    while (LL_RCC_HSI_IsReady() != 1) {}
+	
+    /* Configure PLL */
     LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_8, 216, LL_RCC_PLLP_DIV_2);
     LL_RCC_PLL_Enable();
-
-    /* Wait till PLL is ready */
-    while (LL_RCC_PLL_IsReady() != 1) { }
+    while (LL_RCC_PLL_IsReady() != 1) {}
+	
+	/* Set prescalers */
     LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
     LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_4);
     LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_2);
+	
+    /* Configure system clock */
     LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-
-    /* Wait till System clock is ready */
-    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) { }
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) {}
+	
+	/* Configure systick */
     LL_Init1msTick(216000000);
     LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+    NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 15, 0));
+    LL_SYSTICK_EnableIT();
+	
     LL_SetSystemCoreClock(216000000);
     LL_RCC_SetUSARTClockSource(LL_RCC_USART1_CLKSOURCE_PCLK2);
-
-    /* SysTick_IRQn interrupt configuration */
-    NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 15, 0));
-    LL_SYSTICK_EnableIT();                      /* Enable SysTick interrupts */
 }
 
 /**
