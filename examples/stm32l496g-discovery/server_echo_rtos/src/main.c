18ee69b373c461b4c2b49ee210065660e173a34c
tilz0R <tilen.majerle@st.com>
2019-02-05 12:09:32 +0100

    Add missing parameters for server

--- examples/stm32l496g-discovery/server_echo_rtos/src/main.c
+++ examples/stm32l496g-discovery/server_echo_rtos/src/main.c
@@ -195,7 +195,7 @@ esp_callback_func(esp_evt_t* evt) {
         case ESP_EVT_WIFI_CONNECTED: {
             /* Start server on port 80 and set callback for new connections */
             printf("Wifi connected\r\n");
-            if ((res = esp_set_server(1, 80, ESP_CFG_MAX_CONNS, 100, server_callback_func, 0)) == espOK) {
+            if ((res = esp_set_server(1, 80, ESP_CFG_MAX_CONNS, 100, server_callback_func, NULL, NULL, 0)) == espOK) {
                 printf("Starting server on port 80..\r\n");
             } else {
                 printf("Cannot start server on port 80..\r\n");
@@ -205,7 +205,7 @@ esp_callback_func(esp_evt_t* evt) {
         case ESP_EVT_WIFI_DISCONNECTED: {
             /* Stop server on port 80, others parameters are don't care */
             printf("Wifi disconnected\r\n");
-            if ((res = esp_set_server(0, 80, ESP_CFG_MAX_CONNS, 100, server_callback_func, 0)) == espOK) {
+            if ((res = esp_set_server(0, 80, ESP_CFG_MAX_CONNS, 100, server_callback_func, NULL, NULL, 0)) == espOK) {
                 printf("Disabling server on port 80..\r\n");
             } else {
                 printf("Cannot disable server on port 80..\r\n");
