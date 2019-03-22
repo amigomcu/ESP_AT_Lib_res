4abe6fa976656ee0f5d8858bac2059d952eff801
tilz0R <tilen.majerle@st.com>
2019-03-22 16:34:48 +0100

    Update docs

--- docs/src/_faq.h
+++ docs/src/_faq.h
@@ -4,12 +4,12 @@
  *
  * \section         sect_faq_min_at_sw What is the minimum ESP8266 AT software version?
  *
- * This library follows latest AT official releases available on official Espressif website.
+ * Library follows latest AT releases available on official Espressif website.
  *
  *  - `1.7.0` AT command version is mandatory: https://www.espressif.com/en/products/software/esp-at/resource
- * 	- If you are not sure about your version, use `AT+GMR\r\n` command to test for current AT version
+ * 	- Use `AT+GMR\r\n` command to query current AT version running on ESP device
  *
- * \note            In case you do not fulfill these part, please update software on ESP to latest. Check \ref page_update_at_software section.
+ * \note            If you do not match these requirements, please update software on ESP to the latest one. Check \ref page_update_at_software section.
  *
  * \section         sect_faq_can_rtos Can I use this library with operating system?
  *
